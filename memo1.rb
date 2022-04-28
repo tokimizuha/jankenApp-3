8bf73422f8301559087bbc1ffb4a06632c7b8ef5
tokimizuha <dearl.g.s.lacie0722@yahoo.ne.jp>
2022-04-28 15:14:37 +0000

    じゃんけん&あっち向いてホイのアプリ作成

---
+++ memo1.rb
@@ -0,0 +1,103 @@
+puts "じゃんけん..."
+puts "0（グー）1（チョキ）2（パー）"
+
+def janken_time
+
+my_hand = gets.to_i
+program_hand = rand(2)
+
+janken = ["グーを出しました","チョキを出しました","パーを出しました"]
+
+puts "ホイ！"
+puts"-----------------"
+puts "あなた:#{janken[my_hand]}, 相手:#{janken[program_hand]}"
+puts "----------------"
+
+win = (my_hand == 0 && program_hand == 1) || (my_hand == 1 && program_hand == 2) || (my_hand == 2 && program_hand == 0)
+lose = !(win)
+
+if my_hand == program_hand
+    puts "あいこで"
+    puts "0（グー）1（チョキ）2（パー）"
+    return true
+    
+  elsif win
+    puts "あなたの勝ちです。あっち向いてホイに移行します"
+    @result = "win"
+    puts ""#ここにputsを入れると正常に稼働した
+
+  elsif lose
+    puts "あなたの負けです。あっち向いてホイに移行します"
+    @result = "lose"
+    puts ""#ここにputsを入れると正常に稼働した
+end
+  
+end
+
+next_game = true
+
+while next_game
+   next_game = janken_time
+end
+
+
+
+def acchimuite_hoi_time
+
+if @result == "win"
+   puts "あっち向いて〜"
+   puts "0（上）1（下）2（左）3（右）"
+
+   my_hand_2 = gets.to_i
+   program_hand_2 = rand(3)
+
+   acchimuite_hoi = ["上","下","左","右"]
+
+   puts "ホイ！"
+   puts "-----------------"
+   puts "あなた:#{acchimuite_hoi[my_hand_2]}, 相手:#{acchimuite_hoi[program_hand_2]}"
+   puts "-----------------"
+   
+   if my_hand_2 == program_hand_2
+      puts "あなたの勝ちです"
+    
+  　elsif my_hand_2 != program_hand_2
+      puts "やり直しです"
+      return true
+    
+   end
+   
+end
+
+if @result == "lose"
+   puts "あっち向いて〜"
+   puts "0（上）1（下）2（左）3（右）"
+  
+   my_hand_2 = gets.to_i
+   program_hand_2 = rand(3)
+  
+   acchimuite_hoi = ["上","下","左","右"]
+  
+   puts "ホイ！"
+   puts"-----------------"
+   puts "あなた:#{acchimuite_hoi[my_hand_2]}, 相手:#{acchimuite_hoi[program_hand_2]}"
+   puts "----------------"
+   
+   if my_hand_2 == program_hand_2 
+      puts "あなたの負けです"
+      
+    elsif my_hand_2 != program_hand_2 
+      puts "やり直しです"
+      return true
+    　
+   end
+   
+end
+
+end
+
+next_game_2 = true
+
+while next_game_2
+   next_game_2 = acchimuite_hoi_time
+end
